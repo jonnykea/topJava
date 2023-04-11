eb3aa91924b2b718920cc8c8f3682fe297177155
jonnykea <jonny.kea@mail.ru>
2023-04-11 16:39:06 +1000

    item 3 - fix test for meals and carry common code out in init-i18

--- src/test/java/ru/javawebinar/topjava/UserTestData.java
+++ src/test/java/ru/javawebinar/topjava/UserTestData.java
@@ -29,6 +29,7 @@ public class UserTestData {
     public static final User user = new User(USER_ID, "User", "user@yandex.ru", "password", Role.USER);
     public static final User admin = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ADMIN, Role.USER);
     public static final User guest = new User(GUEST_ID, "Guest", "guest@gmail.com", "guest");
+    public static final User unauthorised = new User(NOT_FOUND, "not", "not", "");
 
     static {
         user.setMeals(meals);
